116bb8a3d4fc4770bc6a1110bd7822ddb684270a
Josseph Peralta <138347140+LeviPeralta@users.noreply.github.com>
2025-10-20 07:33:28 -0600

    Delete U2_TI_09_ListaCanciones directory

--- U2_TI_09_ListaCanciones/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Playlist myPlaylist = new Playlist();
-
-        myPlaylist.addSong("Yesterday", 150);
-        myPlaylist.addSong("Imagine", 180);
-        myPlaylist.printPlaylist();
-    }
-}
