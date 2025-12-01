1865b85300b52f27d36c99199b27855db80330f4
Josseph Peralta <138347140+LeviPeralta@users.noreply.github.com>
2025-12-01 09:49:52 -0600

    Add files via upload

---
+++ Apuntes-main/demo/src/main/java/demo/demo/dto/RequestCadena.java
@@ -0,0 +1,19 @@
+package demo.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class RequestCadena {
+      
+    String cadena;
+      
+    public String getCadena(){
+
+        return cadena;
+    }
+
+    public String setCadena(){
+         return cadena;
+    }
+        
+}
